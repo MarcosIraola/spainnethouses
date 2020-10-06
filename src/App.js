f46a48e732d600af08cdd78ae4a572fb2963d019
dani <maildedani9@gmail.com>
2020-10-06 10:29:38 +0200

    header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Footer from './components/footer/footer.view';
 import ListHouse from './components/listHouse/listHouse.view';
 import NavBar from './components/navbar/navbar.view';
 import Landing from './pages/landing/landing.view';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Header />
       <Landing/>
       <Footer/> 
     </div>
---
+++ src/components/header/header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Divide } from 'react-feather';
+import styles from './header.module.css';
+
+
+
+const Header = () => {
+
+
+    return (
+        <div className={styles.div_header}>
+            <div className={styles.div_finder}>
+                <label for="tipo">Que buscas...</label>
+                <select id="tipo">
+                    <option value="venta">venta</option>
+                    <option value="alquiler">alquiler</option>
+                    <option value="otros">otros</option>
+                </select>
+                <label for="room">Habitaciones</label>
+                <select id="room">
+                    <option >1</option>
+                    <option >2</option>
+                    <option >3</option>
+                    <option >4+</option>
+                </select>
+                <label for="ciudad">Elije ciudad</label>
+                <select  id="ciudad">
+                    <option value="">Barcelona</option>
+                    <option value="">Madrid</option>
+                    <option value="">Sevilla</option>
+                    <option value="">Bilbao</option>
+                </select>
+            </div>
+
+
+        </div>
+
+
+    )
+    
+}
+
+export default Header;
